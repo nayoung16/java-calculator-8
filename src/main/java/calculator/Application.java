2ed9095851288f616efb854f7f11d4ded251cf3a
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-10-19 17:55:26 +0900

    feat: 기본 구분자 숫자 합 반환 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,37 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
+        System.out.println("결과 : " + add(inputString));
+    }
+
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String delimiter = "[,:]";
+
+        String[] tokens = input.split(delimiter);
+        int sum = 0;
+
+        for (String token : tokens) {
+            if (token.isEmpty()) continue;
+
+            try {
+                int num = Integer.parseInt(token);
+                if (num < 0) {
+                    throw new IllegalArgumentException("음수는 허용되지 않습니다: " + num);
+                }
+                sum += num;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다: " + token);
+            }
+        }
+
+        return sum;
     }
 }
